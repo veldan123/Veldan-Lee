994581d5e5672ae4d5733465dffa8a0df910695e
veldan123 <161025728+veldan123@users.noreply.github.com>
2025-02-17 19:57:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function b1(){
+    window.open("https://code.org/") 
+}
+function b2(){
+    window.open("https://codepen.io/") 
+}
+function b3(){
+    window.open("https://code.visualstudio.com/") 
+}
+
+function b4(){
+    window.open("https://appinventor.mit.edu/") 
+}
+
+
+function b5(){
+    window.open("https://github.com/")                                                        
+}
+
+
+
+
+
